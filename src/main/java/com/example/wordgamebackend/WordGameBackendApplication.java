8188351b8be7a81b1034f5c095b8178b94a4a713
TomCorson <corsontom@yahoo.com>
2021-03-08 12:15:51 -0500

    static variables for test controller

--- src/main/java/com/example/wordgamebackend/WordGameBackendApplication.java
+++ src/main/java/com/example/wordgamebackend/WordGameBackendApplication.java
@@ -2,6 +2,7 @@ package com.example.wordgamebackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class WordGameBackendApplication {
--- src/test/java/com/example/wordgamebackend/Controllers/MadLibControllerTest.java
+++ src/test/java/com/example/wordgamebackend/Controllers/MadLibControllerTest.java
@@ -1,8 +1,25 @@
 package com.example.wordgamebackend.Controllers;
 
+import com.example.wordgamebackend.WordGameBackendApplication;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import static org.junit.Assert.*;
-
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = WordGameBackendApplication.class)
+@WebAppConfiguration
 public class MadLibControllerTest {
+
+    public static final long MADLIB_1_ID = 1l;
+    public static final long MADLIB_2_ID = 2l;
+    public static final long MADLIB_3_ID = 3l;
+    public static final String MADLIB_1_NAME = "One Name";
+    public static final String MADLIB_2_NAME = "Two Name";
+    public static final String MADLIB_3_NAME = "Three Name";
+    public static final String MADLIB_1_STORY = "One Story";
+    public static final String MADLIB_2_STORY = "Two Story";
+    public static final String MADLIB_3_STORY = "Three Story";
 
     @org.junit.Before
     public void setUp() throws Exception {
