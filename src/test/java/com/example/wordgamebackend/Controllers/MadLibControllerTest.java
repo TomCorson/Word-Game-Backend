1bc01067a29ab3a6cde554ddff3887890285b68e
TomCorson <corsontom@yahoo.com>
2021-03-08 11:56:30 -0500

    test controller class

---
+++ src/test/java/com/example/wordgamebackend/Controllers/MadLibControllerTest.java
@@ -0,0 +1,30 @@
+package com.example.wordgamebackend.Controllers;
+
+import static org.junit.Assert.*;
+
+public class MadLibControllerTest {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+    }
+
+    @org.junit.Test
+    public void getMadLib() {
+    }
+
+    @org.junit.Test
+    public void getAllMadLib() {
+    }
+
+    @org.junit.Test
+    public void createMadLib() {
+    }
+
+    @org.junit.Test
+    public void deleteMadLib() {
+    }
+}
